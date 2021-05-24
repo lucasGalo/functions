1ad07c4902eea824d3c10bcc715543f7aa7ad984
lucas_galo <lucas_d>
2021-05-24 20:45:29 -0300

    BiFunction, Function

--- src/main/java/abs/Abs.java
+++ src/main/java/abs/Abs.java
@@ -5,7 +5,9 @@ import java.util.function.Function;
 
 public class Abs<T, U, R, J> {
 
-  public J powToString(T a1, U a2, BiFunction<T, U, R> biFunction, Function<R, J> function) {
+  public J powToString(T a1, U a2,
+                       BiFunction<T, U, R> biFunction,
+                       Function<R, J> function) {
         return biFunction.andThen(function).apply(a1, a2);
 
     }
--- src/main/java/biFunction/statico.java
+++ src/main/java/biFunction/statico.java
@@ -1,5 +1,6 @@
 package biFunction;
 
+import abs.Abs;
 import abs.IAbs;
 
 import java.util.function.BiFunction;
@@ -16,9 +17,21 @@ public class statico {
                 Math::pow,
                 (r) -> "Result : " + r);
 
+        BiFunction<String, String,String> bi = (x, y) ->  x + y ;
+        Function<String,String> fu = x-> x+" where codigo = 1";
+        Abs absG = new Abs<String, String, String, String>();
+
+        String o = (String) absG.powToString("select *", " from tabela",
+                (x, y) -> x + "" + y,
+                (x) -> x + " where codigo = 1");
+
+        System.out.println(o);
+
+
         String result = powToString(2, 4,
                 Math::pow, //(a1, a2) -> Math.pow(a1, a2),
                 (r) -> "Result : " + r);
+
 
         System.out.println(result); // Result : 16.0
 
